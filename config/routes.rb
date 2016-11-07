b55d7072432eeeefa042677ec6398f9dda6cd042
Charles Bryce Steele <stainlesschuck@gmail.com>
2016-11-07 15:42:28 -0600

    Part 2 wrap-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'finder#index'
+  get 'missing_email' => 'finder#missing_email'
+  get 'alphabetized' => 'finder#alphabetized'
+
 end
